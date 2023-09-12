89ccf61d00960f0fe6ed81b332dd22ccc9296c92
Yong706 <102671899+Yong706@users.noreply.github.com>
2023-09-12 14:22:48 +0800

    Update script.js

--- Cashier/script.js
+++ Cashier/script.js
@@ -1,49 +1,72 @@
-// JavaScript code for the cash payment simulator
-
+// JavaScript code
 document.addEventListener("DOMContentLoaded", function () {
-  // ... Previous code ...
+    let selectedGender = null;
+    let userInput = 0;
+    let randomAmount = generateRandomAmount();
 
-  // Handle money selection
-  function handleMoneySelection(amount) {
-    // Check if the game is currently active
-    if (isPlaying) {
-      // Add the selected amount to the total
-      totalAmount += amount;
+    // Gender Selection Logic
+    document.getElementById("boyBtn").addEventListener("click", function () {
+        selectedGender = "boy";
+        // Add any logic or styling for the boy selection if needed
+    });
 
-      // Update the display to show the current totalAmount
-      updateTotalDisplay();
+    document.getElementById("girlBtn").addEventListener("click", function () {
+        selectedGender = "girl";
+        // Add any logic or styling for the girl selection if needed
+    });
 
-      // Check if the totalAmount matches the question amount
-      const questionAmount = parseFloat(questionText.textContent.split("RM")[1]);
-      if (totalAmount === questionAmount) {
-        handleCorrectAnswer(); // Handle a correct answer
-      }
+    // Generating Random Amount
+    function generateRandomAmount() {
+        return (Math.random() * 30).toFixed(2);
     }
-  }
 
-  // Example function to update the total amount display
-  function updateTotalDisplay() {
-    // Update the display element with the total amount
-    const totalAmountElement = document.getElementById("total-amount-element");
-    totalAmountElement.textContent = `Total Amount: RM ${totalAmount.toFixed(2)}`;
-  }
+    // Printing the Question
+    function printQuestion(amount) {
+        document.getElementById("questionText").textContent = `Which combination of money is correct for RM ${amount}?`;
+    }
 
-  // Example function to handle a correct answer
-  function handleCorrectAnswer() {
-    // Display a congratulatory message
-    const congratsMessage = document.createElement("p");
-    congratsMessage.textContent = "Congrats, you are correct.";
-    document.body.appendChild(congratsMessage);
+    // Handling User Input
+    document.querySelectorAll(".money-button").forEach(function (button) {
+        button.addEventListener("click", function () {
+            const value = parseFloat(button.value);
+            userInput += value;
+        });
+    });
 
-    // Optionally, you can proceed to the next question or perform other actions here
-    // For example, you can call a function to load the next question
-    loadNextQuestion();
-  }
+    document.getElementById("submitBtn").addEventListener("click", function () {
+        if (userInput === parseFloat(randomAmount)) {
+            document.getElementById("resultText").textContent = "Congrats, the answer is correct!";
+            // Show the Next Question and Exit buttons
+        } else {
+            document.getElementById("resultText").textContent = "Sorry, the answer is wrong. Please try again.";
+        }
+    });
 
-  // Example function to load the next question (you can replace this with your logic)
-  function loadNextQuestion() {
-    // Implement logic to load the next question here
-    // For example, you can generate a new random question and update the question modal
-    showRandomQuestion();
-  }
+    // Next Question and Exit
+    document.getElementById("nextBtn").addEventListener("click", function () {
+        userInput = 0;
+        randomAmount = generateRandomAmount();
+        printQuestion(randomAmount);
+        document.getElementById("resultText").textContent = "";
+        // Hide the Next Question and Exit buttons
+    });
+
+    document.getElementById("exitBtn").addEventListener("click", function () {
+        const confirmExit = confirm("Are you sure you want to exit the game?");
+        if (confirmExit) {
+            window.location.href = "https://www.google.com"; // Replace with your exit URL
+        }
+    });
+
+    // Photo Upload Logic
+    const photoLink = document.getElementById("photoLink");
+
+    photoLink.addEventListener("click", function (event) {
+        event.preventDefault();
+        const userPhotoLink = prompt("Enter your photo link:");
+
+        if (userPhotoLink) {
+            photoLink.href = userPhotoLink;
+        }
+    });
 });
