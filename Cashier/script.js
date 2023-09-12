b661e41f4100e3dc261141ab76e0d64001785f96
Yong706 <102671899+Yong706@users.noreply.github.com>
2023-09-12 13:41:48 +0800

    Update script.js

--- Cashier/script.js
+++ Cashier/script.js
@@ -1,83 +1,49 @@
 // JavaScript code for the cash payment simulator
 
 document.addEventListener("DOMContentLoaded", function () {
-  const welcomeScreen = document.getElementById("welcome-screen");
-  const gameScreen = document.getElementById("game-screen");
-  const boyButton = document.getElementById("boy-button");
-  const girlButton = document.getElementById("girl-button");
-  const playButton = document.getElementById("play-button");
-  const questionModal = document.getElementById("question-modal");
-  const questionText = document.getElementById("question-text");
-  const closeButton = document.getElementById("close-button");
-  const moneyContainer = document.getElementById("money-container");
-  const oneCentButton = document.getElementById("one-cent-button");
-
-  let selectedCharacter = "";
-  let isPlaying = false;
-
-  // Character selection
-  boyButton.addEventListener("click", () => {
-    selectedCharacter = "Boy";
-    startGame();
-  });
-
-  girlButton.addEventListener("click", () => {
-    selectedCharacter = "Girl";
-    startGame();
-  });
-
-  function startGame() {
-    welcomeScreen.classList.add("hidden");
-    gameScreen.classList.remove("hidden");
-    isPlaying = true;
-    showRandomQuestion();
-  }
-
-  // Question modal
-  closeButton.addEventListener("click", () => {
-    questionModal.classList.add("hidden");
-    if (isPlaying) {
-      showRandomQuestion();
-    }
-  });
-
-  function showRandomQuestion() {
-    // Generate and display a random question
-    const randomAmount = (Math.random() * 30).toFixed(2);
-    questionText.textContent = `How much is RM ${randomAmount}?`;
-    questionModal.classList.remove("hidden");
-  }
-
-  // Money denominations
-  const moneyDenominations = [
-    { value: 1.00, text: "RM 1", image: "rm1.png" }, // Replace with your image file path
-    { value: 5.00, text: "RM 5", image: "rm5.png" }, // Replace with your image file path
-    { value: 10.00, text: "RM 10", image: "rm10.png" }, // Replace with your image file path
-    { value: 20.00, text: "RM 20", image: "rm20.png" }, // Replace with your image file path
-  ];
-
-  // Populate the money container with buttons and images
-  moneyDenominations.forEach((denomination) => {
-    const button = document.createElement("button");
-    button.className = "money-denomination";
-    button.textContent = denomination.text;
-    button.addEventListener("click", () => handleMoneySelection(denomination.value));
-    moneyContainer.appendChild(button);
-
-    // Create and append an image element
-    const img = document.createElement("img");
-    img.src = denomination.image; // Change to your image file path
-    img.alt = denomination.text;
-    moneyContainer.appendChild(img);
-  });
-
-  // 1 Cent button
-  oneCentButton.addEventListener("click", () => handleMoneySelection(0.01));
+  // ... Previous code ...
 
   // Handle money selection
   function handleMoneySelection(amount) {
-    // Handle the logic for adding the selected amount to the game
-    // You can keep track of the total amount, check if it matches the question, etc.
-    // Update the game state accordingly
+    // Check if the game is currently active
+    if (isPlaying) {
+      // Add the selected amount to the total
+      totalAmount += amount;
+
+      // Update the display to show the current totalAmount
+      updateTotalDisplay();
+
+      // Check if the totalAmount matches the question amount
+      const questionAmount = parseFloat(questionText.textContent.split("RM")[1]);
+      if (totalAmount === questionAmount) {
+        handleCorrectAnswer(); // Handle a correct answer
+      }
+    }
+  }
+
+  // Example function to update the total amount display
+  function updateTotalDisplay() {
+    // Update the display element with the total amount
+    const totalAmountElement = document.getElementById("total-amount-element");
+    totalAmountElement.textContent = `Total Amount: RM ${totalAmount.toFixed(2)}`;
+  }
+
+  // Example function to handle a correct answer
+  function handleCorrectAnswer() {
+    // Display a congratulatory message
+    const congratsMessage = document.createElement("p");
+    congratsMessage.textContent = "Congrats, you are correct.";
+    document.body.appendChild(congratsMessage);
+
+    // Optionally, you can proceed to the next question or perform other actions here
+    // For example, you can call a function to load the next question
+    loadNextQuestion();
+  }
+
+  // Example function to load the next question (you can replace this with your logic)
+  function loadNextQuestion() {
+    // Implement logic to load the next question here
+    // For example, you can generate a new random question and update the question modal
+    showRandomQuestion();
   }
 });
